8b5c25d668a4ce828c034a025e0350ef7d0c7045
Marco Mechini <mechinimarco35@gmail.com>
2024-11-12 16:01:10 +0100

    feat: es 2 complete

---
+++ es2/script.js
@@ -0,0 +1,39 @@
+const team = [
+    {
+        nome: 'Parma',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Venezia',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Frosinone',
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+];
+
+const newArray = [];
+
+const getRand = () => Math.floor(Math.random() * 10 + 1);
+
+
+for (let i = 0; i < team.length; i++) {
+
+    const curTeam = team[i];
+    const curTeamName = curTeam.nome;
+    const curTeamPenal = getRand();
+    const curTeamPoint = getRand();
+
+    //Invece di assegnare il valore facevo push del risultato e incremendando l'array mi diventava un loop infinito
+    team[i].puntiFatti = curTeamPenal;
+    team[i].falliSubiti = curTeamPoint;
+
+    newArray.push({ nome: curTeamName, falliSubiti: curTeamPenal });
+}
+console.log(team);
+console.log(newArray);
+
